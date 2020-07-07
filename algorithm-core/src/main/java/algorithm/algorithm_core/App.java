4abc4c157c815bf52263b0f207606e813c5ee65c
cony <cony@cony>
2020-07-07 21:42:20 +0900

    create python package

--- algorithm-core/src/main/java/algorithm/algorithm_core/App.java
+++ algorithm-core/src/main/java/algorithm/algorithm_core/App.java
@@ -1,13 +1,41 @@
 package algorithm.algorithm_core;
 
+import java.util.Date;
+import java.util.Objects;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+    private static int a;
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+       Date start = new Date();
+       Date end = new Date();
+       Period p = new Period(start, end);
+       end.setYear(78);
+
+       p.end().toString();
     }
+
+
+}
+
+final class Period{
+    private final Date start;
+    private final Date end;
+
+    public Period(Date start, Date end){
+        this.start = start;
+        this.end = end;
+    }
+    public Date start(){
+        return start;
+    }
+    public Date end(){
+        return end;
+    }
+
 }
